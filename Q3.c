3576b919522b2bcd6f2def474feab65c447fdbeb
Joselito <motajoselito@hotmail.com>
2014-04-06 19:35:02 -0300

    Questao 03

---
+++ Q3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int i;
+
+  printf("Digite um numero\n");
+  scanf("%d",&i);
+  printf("O numero digitado foi o %d",i);
+
+  system("pause");
+  return 0;
+
+
+}
