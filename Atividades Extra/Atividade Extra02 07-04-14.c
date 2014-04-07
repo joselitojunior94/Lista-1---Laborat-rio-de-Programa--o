1b7e81e02c7bd49cddc09b5a7eec8680ba27cf11
Joselito <motajoselito@hotmail.com>
2014-04-07 18:37:42 -0300

    Atividades Extra

---
+++ Atividades Extra/Atividade Extra02 07-04-14.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int a = 10, b = 1, c = 12;
+
+if(a>b+c){
+    printf("Verdadeiro\n");
+  }else{
+    printf("Falso\n");
+  }
+
+  system("pause");
+   return 0;
+
+
+
+
+}
