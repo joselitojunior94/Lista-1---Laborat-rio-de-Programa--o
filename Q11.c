cad510d5c69c1afbf049d7af4f2f0ab96ffe16c5
Joselito <motajoselito@hotmail.com>
2014-04-06 22:18:31 -0300

    Questao 11

---
+++ Q11.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    float area;
+
+
+  printf("Digite o valor do lado\n");
+  scanf("%f",&area);
+
+  area = (pow(area,2);
+
+ printf("O dobro da area do quadrado eh %.2f\n",area*2);
+
+  system("pause");
+  return 0;
+
+}
+
+
