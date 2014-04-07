a1ff88112e08c35e7acc48191fbfa183dcd3170f
Joselito <motajoselito@hotmail.com>
2014-04-06 22:19:37 -0300

    Questao 07

---
+++ Q7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   int num1;
+   int num2;
+
+  printf("Digite o primeiro numero\n");
+  scanf("%d",&num1);
+  printf("Digite o segundo numero\n");
+  scanf("%d",&num2);
+
+  printf("A soma entre eles eh: %d\n",num1+num2);
+
+  system("pause");
+  return 0;
+
+
+}
