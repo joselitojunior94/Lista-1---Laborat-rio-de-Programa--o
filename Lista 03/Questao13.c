576c2092de8cc083e6e6b282fdccaed3704614ff
Joselito <motajoselito@hotmail.com>
2014-05-04 17:34:28 -0300

    Questao13

---
+++ Lista 03/Questao13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int i;
+  i = 10;
+  while(i!=0){
+    printf("%d\n\n\n",i);
+    i--;
+  }
+ printf("\n\n\n");
+ printf("FIM\n\n");
+ system("pause");
+ return 0;
+
+
+
+}
