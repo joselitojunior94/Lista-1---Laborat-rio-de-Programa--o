0b95e1d22dc4fcf9f6e79d8f743906118db1d86c
Joselito <motajoselito@hotmail.com>
2014-05-17 23:55:37 -0300

    .

---
+++ Lista 04/Questao11.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void main(){
+    char string01[1000],string02[1000];
+    int i,tam01,tam02;
+
+   printf("Digite uma string\n");
+   scanf("%s",&string01);
+   tam01 = strlen(string01);
+   printf("Digite uma string\n");
+   scanf("%s",&string02);
+   tam02 = strlen(string02);
+   for(i=1;i<=tam01;i++){
+     if(string01[i] == 'a' || string01[i] == 'A'){
+        string01[i] = '*';
+     }
+   }
+    for(i=1;i<=tam02;i++){
+     if(string02[i] == 'a' || string02[i] == 'A'){
+        string02[i] = '*';
+     }
+   }
+
+  printf("A string 01 com a troca de a por * e  invertida eh:  %s\n",strrev(string01));
+  printf("A string 02 com a troca de a por * e  invertida eh:  %s\n",strrev(string02));
+
+}
