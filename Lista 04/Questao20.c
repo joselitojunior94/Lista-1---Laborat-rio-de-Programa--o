0ae9ebe028ded1c629c236d96db4531d70c986eb
Joselito <motajoselito@hotmail.com>
2014-05-18 01:35:11 -0300

    .

---
+++ Lista 04/Questao20.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void vogais (){
+
+ char palavra[10000];
+
+
+     printf("Digite uma palavra\n");
+     scanf("%s",&palavra);
+     printf("\n A palavra eh: %s \n",palavra);
+
+
+}
+
+void main(){
+ vogais();
+}
