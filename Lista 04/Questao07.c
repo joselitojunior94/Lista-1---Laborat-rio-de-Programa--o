869c1f14f50ae5319ccefdd68ede862664a66b1f
Joselito <motajoselito@hotmail.com>
2014-05-17 22:32:07 -0300

    .

---
+++ Lista 04/Questao07.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void main(){
+     char palavra[1000];
+     int i,j,cont;
+
+     printf("Digite uma palavra\n");
+    scanf("%s",&palavra);
+
+  for(i=0;palavra[i]!= '\0';i++){
+   if(palavra[i] != ' '){
+    if((palavra[i] =='a') || (palavra[i] =='e') || (palavra[i] =='i') || (palavra[i] =='o') || (palavra[i] =='u')){
+      for(j=i;j<=strlen(palavra);j++){
+       palavra[j]  = palavra[j+1];
+      }
+    }
+  }
+} printf("%s",palavra);
+}
